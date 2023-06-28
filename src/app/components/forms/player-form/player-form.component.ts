8a565ab4ca9cf08acdce37b7a84650d40b3bab58
Nathan Chapelle <ncdev@outlook.fr>
2023-06-28 20:35:17 +0200

    up tireness of players

--- src/app/components/forms/player-form/player-form.component.ts
+++ src/app/components/forms/player-form/player-form.component.ts
@@ -1,40 +1,41 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup} from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 import { PlayerInterface } from 'src/app/Interfaces/player.interface';
 
 @Component({
   selector: 'app-player-form',
   templateUrl: './player-form.component.html',
-  styleUrls: ['./player-form.component.scss']
+  styleUrls: ['./player-form.component.scss'],
 })
-export class PlayerFormComponent implements OnInit{
-
+export class PlayerFormComponent implements OnInit {
   @Output() playerFormEvent = new EventEmitter();
   player1Form: FormGroup;
   player2Form: FormGroup;
 
-  constructor(){}
+  constructor() {}
 
   ngOnInit(): void {
     this.player1Form = new FormGroup({
       name: new FormControl('Joueur1'),
       level: new FormControl(5),
-    })
+    });
 
     this.player2Form = new FormGroup({
       name: new FormControl('Joueur2'),
-      level: new FormControl(5)
-    })
+      level: new FormControl(5),
+    });
   }
 
-  onSubmit(player1: PlayerInterface, player2: PlayerInterface): void{
+  onSubmit(player1: PlayerInterface, player2: PlayerInterface): void {
     this.strengthCalculator(player1);
     this.strengthCalculator(player2);
-    
-    this.playerFormEvent.emit([player1, player2])    
+
+    this.playerFormEvent.emit([player1, player2]);
   }
 
   private strengthCalculator(player: PlayerInterface) {
-      player.strength = (player.level * 10 - (player.level / 2)) - Math.floor(Math.random() * 11);
+    let strength =
+      player.level * 10 - player.level / 2 - Math.floor(Math.random() * 21);
+    strength < 10 ? (player.strength = 10) : (player.strength = strength);
   }
 }

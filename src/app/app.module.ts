35254e9b4c7ac48e7a360e6f7313a5edff36c4f0
Nathan Chapelle <ncdev@outlook.fr>
2023-06-28 11:31:37 +0200

    added a player form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { PlayerFormComponent } from './components/forms/player-form/player-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    PlayerFormComponent
   ],
   imports: [
     BrowserModule,

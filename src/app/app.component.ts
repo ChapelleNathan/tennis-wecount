42075b8cae453f104b4f43532063b9d4f2fd198e
Nathan Chapelle <ncdev@outlook.fr>
2023-06-28 20:48:10 +0200

    fixed bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,8 @@ import { PlayerInterface } from './Interfaces/player.interface';
 export class AppComponent implements OnInit {
   playerDatas: Array<PlayerInterface>;
   gameResults: Array<string>;
+  player1Score: number;
+  player2Score: number;
 
   ngOnInit(): void {
     this.playerDatas = null;
@@ -24,8 +26,8 @@ export class AppComponent implements OnInit {
   private game(players: Array<PlayerInterface>): Array<string> {
     let player1 = players[0];
     let player2 = players[1];
-    let player1Score: number = 0;
-    let player2Score: number = 0;
+    this.player1Score = 0;
+    this.player2Score = 0;
     let dice: number;
     let results = [];
     for (let i = 0; i < 150; i++) {
@@ -33,17 +35,17 @@ export class AppComponent implements OnInit {
         dice = this.diceRoll();
         if (player1.strength < dice) {
           results.push('Point ' + (i + 1) + ' : remporté par ' + player1.name);
-          player2Score++;
+          this.player2Score++;
           break;
         }
         dice = this.diceRoll();
         if (player2.strength < dice) {
           results.push('Point ' + (i + 1) + ' : remporté par ' + player2.name);
-          player1Score++;
+          this.player1Score++;
           break;
         }
       }
-    }
+    }    
     return results;
   }
 

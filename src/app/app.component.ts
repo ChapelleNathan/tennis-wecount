6ec4d51feedff44d717019d84c7a698806a5a5fc
Nathan Chapelle <ncdev@outlook.fr>
2023-06-29 11:36:16 +0200

    display only 5 sets for a complete game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { PlayerInterface } from './Interfaces/player.interface';
 import { GameInterface } from './Interfaces/game.interface';
 import { SetInterface } from './Interfaces/set.interface';
+import { find } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -44,31 +45,51 @@ export class AppComponent implements OnInit {
 
   private play(player1: PlayerInterface, player2: PlayerInterface): void {
     let lastSet: SetInterface;
-    //initialisation de la game
+    //initialisation de la partie
     this.initGame(player1, player2);
+    this.newSet();
+    lastSet = this.game.sets[this.game.sets.length - 1];
+
     //on boucle pour avoir le nombre d'échange de coup souhaité
-    for (let i = 0; i < 1; i++) {
-      this.newSet();
-      let lastSet = this.game.sets[this.game.sets.length - 1];
-      this.newPoint(lastSet.players[0], lastSet.players[1]);
+    for (let i = 0; i < 150; i++) {
+
+      //On vérifie si le nombre de set gagné correspond au nombre de set total si c'est le cas il faut créer un nouveau set
+      if(this.game.players[0].setPoint + this.game.players[1].setPoint == this.game.sets.length) {
+        if(this.game.sets.length > 5) {
+          this.game.sets.pop();
+          return;
+        }
+        lastSet = this.game.sets[this.game.sets.length - 1];
+        this.newSet();
+      } 
+
+      this.newPoint(lastSet.players[0], lastSet.players[1],i + 1);
     }
   }
 
   private newPoint(
     shooter: { player: PlayerInterface; score: number },
-    opponent: { player: PlayerInterface; score: number }
+    opponent: { player: PlayerInterface; score: number },
+    pointNumber: number,
   ) {
     //on lance le dé pour savoir si il réussi son coup
     let roll = this.diceRoll();
+
     if (shooter.player.strength > roll) {
       //si il réussi son coup, il l'envoi à opponent qui deviens le shooter
-      this.newPoint(opponent, shooter);
+      this.newPoint(opponent, shooter, pointNumber);
     } else {
       //si il rate on incrémente le score de opponent de 1
       opponent.score++;
       this.gameLogs.push(
-        opponent.player.name + ' a marqué contre ' + shooter.player.name
+        'Point n°' + pointNumber + ' : ' + opponent.player.name + ' a marqué contre ' + shooter.player.name
       );
+
+      //Si opponent gagne le set on incrémente setPoint de 1
+      if (this.hasWonSet(opponent, shooter)) {
+        let winner = this.game.players.find(el => el.player = opponent.player);
+        winner.setPoint++;
+      }
     }
   }
 
@@ -95,7 +116,7 @@ export class AppComponent implements OnInit {
   ): boolean {
     if (winner.score < 4) {
       return false;
-    } else if (this.game.sets.length === 4 && winner.score < 7) {
+    } else if (this.game.sets.length === 5 && winner.score < 7) {
       return false;
     }
 
